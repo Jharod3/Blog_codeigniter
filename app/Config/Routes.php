aef30f3b60961c8a052eee16b7c9c927b14bc896
JHAROD MIGUEL HIDALGO OSORIO <JHIDALGOO@ANTIOQUIA.GOV.CO>
2024-05-16 15:45:54 -0500

    first commit

---
+++ app/Config/Routes.php
@@ -0,0 +1,11 @@
+<?php
+
+use CodeIgniter\Router\RouteCollection;
+
+/**
+ * @var RouteCollection $routes
+ */
+$routes->group('', ['namespace' => 'App\Controllers\Front'], function($routes){
+    $routes->get('/', 'Home::index');
+
+});
---
+++ app/Views/Front/layout/header.php
@@ -0,0 +1,6 @@
+<section class="hero is-info">
+    <div class="hero-body">
+        <p class="title">Blog personal</p>
+        <p class="subtitle">Lista de entradas</p>
+    </div>
+</section>
